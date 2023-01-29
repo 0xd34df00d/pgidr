7bbaf707f5fda2d461a0a732b53263017b4f2352
0xd34df00d <0xd34df00d@gmail.com>
2023-01-29 13:48:09 -0600

    No need for the C header really

--- cbits/cbits.c
+++ cbits/cbits.c
@@ -1,4 +1,6 @@
-#include "cbits.h"
+#include <stdint.h>
+
+typedef const char *StrPtr;
 
 void setStrArrayItem(StrPtr *array, uint64_t pos, const char *value)
 {
--- cbits/cbits.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-typedef const char *StrPtr;
-
-void setStrArrayItem(StrPtr *array, uint64_t pos, const char *value);
